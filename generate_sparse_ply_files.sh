ef3cb868a0ff9813010ab593107ec0b5c79e7a4b
arthur <arthurlin0120@gmail.com>
2025-05-30 17:10:16 +0000

    sparse case update

---
+++ generate_sparse_ply_files.sh
@@ -0,0 +1,7 @@
+python cv25s_full_pipeline_reloc3r.py --seq_dir /home/arthur/storage/7scenes/chess/test/seq-05/ --output /home/arthur/storage/results/chess-seq-05.ply
+
+python cv25s_full_pipeline_reloc3r.py --seq_dir /home/arthur/storage/7scenes/fire/test/seq-04/ --output /home/arthur/storage/results/fire-seq-04.ply
+
+python cv25s_full_pipeline_reloc3r.py --seq_dir /home/arthur/storage/7scenes/pumpkin/test/seq-07/ --output /home/arthur/storage/results/pumpkin-seq-07.ply
+
+python cv25s_full_pipeline_reloc3r.py --seq_dir /home/arthur/storage/7scenes/stairs/test/seq-04/ --output /home/arthur/storage/results/stairs-seq-04.ply
